2e1cd34b8b9c76cd9d177fff0d02754b3c4c3eca
lichengjie <2835347017@qq.com>
2019-03-10 15:01:12 +0800

    自定义Icon

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: BookIcon(),
       theme: ThemeData(
         primarySwatch: Colors.blue, // 应用主题颜色
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),// 点击后背景高亮的颜色
@@ -19,17 +19,3 @@ class App extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
-
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        body: BookIcon()
-      ),
-    );
-  }
-}
