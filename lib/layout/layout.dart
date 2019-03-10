0c4ed13ef3ef52e244245f4bb28af51ffc8eee2d
lichengjie <2835347017@qq.com>
2019-03-10 17:44:22 +0800

    SizedBox

--- lib/layout/layout.dart
+++ lib/layout/layout.dart
@@ -9,19 +9,34 @@ class LayOut extends StatelessWidget {
       在 Column 里面 主轴是竖着最中间那条线
     * */
     return Column(
-      /*
-        交叉轴 即主轴的垂直平分线
-        crossAxisAlignment
-          CrossAxisAlignment.start 将子对象的起始边与横轴的起始边对齐。
-          CrossAxisAlignment.stretch 交叉轴方向拉伸
-          CrossAxisAlignment.center 放置子对象，使其中心与横轴的中心对齐
-       */
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        CustomIcon(Icons.book, limitSize: 60.0, baseColor: Colors.black12),
-        CustomIcon(Icons.book, limitSize: 60.0, baseColor: Colors.black12),
-        CustomIcon(Icons.book, limitSize: 60.0, baseColor: Colors.black12)
+        ///创建固定大小的框。[width]和[height] 参数可以为空，
+        ///保证框的大小不应限制在相应的维度中。
+        SizedBox(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: CustomIcon(Icons.book, limitSize: 60.0, baseColor: Colors.black12),
+          ),
+        ),
+        /// 作为间隔
+        SizedBox(
+          height: 30.0,
+        ),
+        SizedBox(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: CustomIcon(Icons.book, limitSize: 60.0, baseColor: Colors.black12),
+          ),
+        )
+//        CustomIcon(Icons.book, limitSize: 60.0, baseColor: Colors.black12),
+//        CustomIcon(Icons.book, limitSize: 60.0, baseColor: Colors.black12)
       ],
     );
   }
