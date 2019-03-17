87294232b388e195b193155590504278d0048501
lichengjie <2835347017@qq.com>
2019-03-17 15:41:54 +0800

    Separated

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,6 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue, // 应用主题颜色
-        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),// 点击后背景高亮的颜色
-        splashColor: Colors.white54 // 水波纹yanse
-      ),
     );
   }
 }
@@ -25,11 +20,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        body: MallListView()
-      ),
+    return Scaffold(
+        body: MallListBuilderSeparatView()
     );
   }
 }
--- lib/mall/mall-listview.dart
+++ lib/mall/mall-listview.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../mode/fake_post.dart';
 
 // 独立 _listItemBuilder
-class MallListView extends StatelessWidget {
+class MallListBuilderView extends StatelessWidget {
 
   Widget _listItemBuilder(BuildContext context, int index){
     return Container(
@@ -28,9 +28,52 @@ class MallListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ListView.builder( 
+    return ListView.builder(
+//          reverse: false,
+//          scrollDirection: Axis.vertical,
           itemCount: posts.length,
           itemBuilder: _listItemBuilder,//构建列表视图的项目
         ); // 创建列表视图;
+  }
+}
+
+
+class MallListBuilderSeparatView extends StatelessWidget {
+
+  Widget _listItemBuilder(BuildContext context, int index){
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(4.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0), // 图片下面预留的高度
+          Text(
+              posts[index].title,
+              style:Theme.of(context).textTheme.title
+          ),
+          Text(
+              posts[index].author,
+              style:Theme.of(context).textTheme.subhead
+          ),
+        ],
+      ), // 从上到下排列的容器
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //下划线widget预定义以供复用。
+    Widget divider1=Divider(color: Colors.blue,);
+    Widget divider2=Divider(color: Colors.green);
+    return ListView.separated(
+      itemCount: 3,
+      //列表项构造器
+      itemBuilder: _listItemBuilder,
+      //分割器构造器
+      separatorBuilder: (BuildContext context, int index) {
+        return index%2==0?divider1:divider2;
+      },
+    );
   }
 }
