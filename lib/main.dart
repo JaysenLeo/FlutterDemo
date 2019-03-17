8b25e74dca0cce06eaf122e3d542c8e6eaa40584
lichengjie <2835347017@qq.com>
2019-03-17 15:55:28 +0800

    Scaffold之AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'mall/mall-listview.dart';
 
 void main() => runApp(App());
 
@@ -10,18 +9,40 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
+      theme: ThemeData(
+      ),
     );
   }
 }
 
 class Home extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        body: MallListBuilderSeparatView()
+      // backgroundColor: Colors.orangeAccent,
+      appBar: AppBar(
+        leading: IconButton( // 标题左边区域
+          icon: Icon(Icons.menu),
+          tooltip: '导航',
+          onPressed: () => debugPrint('导航事件'),
+        ),
+        actions: <Widget>[   // 标题右边区域 此处可以设置一组按钮
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: '搜索',
+            onPressed: () => debugPrint('搜索事件'),
+          ),
+           IconButton(
+             icon: Icon(Icons.more_horiz),
+             tooltip: '其他',
+             onPressed: () => debugPrint('其他事件'),
+           )
+        ],
+        title: Text('AppBarTitle'),
+        elevation: 5.0, // 阴影部分
+      ),
+      body: null,
     );
   }
 }
