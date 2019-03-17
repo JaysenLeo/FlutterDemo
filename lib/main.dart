4ac0e6fa917a2977fc4d40e5ad6db43423633e60
lichengjie <2835347017@qq.com>
2019-03-17 14:55:24 +0800

    主题定制

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,39 @@
 import 'package:flutter/material.dart';
-import 'mall/mall-listview.dart';
 
-void main() => runApp(App());
+void main() =>  runApp(App());
 
 class App extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('顶部工具栏标题'),
+            elevation: 10.0, // 设置顶部阴影大小
+          ),// 添加顶部工具栏
+          body: HelloWidget()
+      ),
       theme: ThemeData(
-        primarySwatch: Colors.blue, // 应用主题颜色
-        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),// 点击后背景高亮的颜色
-        splashColor: Colors.white54 // 水波纹yanse
+          primarySwatch: Colors.pink
       ),
     );
   }
 }
 
-class Home extends StatelessWidget {
-
-
+class HelloWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-      // backgroundColor: Colors.orangeAccent,
-        appBar: AppBar(
-        actions: <Widget>[   // 标题右边区域 此处可以设置一组按钮
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: '搜索',
-            onPressed: () => debugPrint('搜索事件'),
+    return Center(
+        child: Text(
+          'Hello Widget',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 45.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.amberAccent,
           ),
-        ],
-        title: Text('AppBarTitle'),
-        elevation: 5.0, // 阴影部分
-      ),
-        body: MallListView()
-      ),
+        )
     );
   }
 }
